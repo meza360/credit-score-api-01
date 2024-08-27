0a471f230aeb43da0274ff02d0ffe817b108f22f
Giovani Meza <giovani.meza@gt2software.dev>
2024-08-27 00:53:32 -0600

    Adds aditional fields to citizen

--- Domain/Relational/Renap/Citizen.cs
+++ Domain/Relational/Renap/Citizen.cs
@@ -3,11 +3,11 @@ namespace Domain.Relational.Renap
     public class Citizen
     {
         public int Id { get; set; }
-        public string CUI { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string CUI { get; set; } = new Random().NextInt64(1000000000000, 9999999999999).ToString();
+        public string FirstName { get; set; } = "John";
+        public string LastName { get; set; } = "Doe";
+        public DateTime DateOfBirth { get; set; } = DateTime.Now.AddYears(-10);
         public DateTime? DateOfDecease { get; set; }
-        public string Nationality { get; set; }
+        public string Nationality { get; set; } = "Guatemala";
     }
 }
